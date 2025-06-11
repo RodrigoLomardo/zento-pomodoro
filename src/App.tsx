5dab6d10ee132d1dfada955c090b56b26c04703e
Rodrigo Lomardo Xisto <rlomardo@detro.rj.gov.br>
2025-06-11 09:51:09 -0300

    Cores configuradas

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,8 @@ export function App() {
 
     return (
         <div>
-            <Heading></Heading>
+            <Heading>
+            </Heading>
 
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio voluptatem corrupti quo itaque rem, in quam dignissimos iste? Ullam quae minus non eum. Cupiditate aspernatur nisi vitae dolore laboriosam? Tempora!</p>
         </div>
--- src/components/Heading.tsx
+++ src/components/Heading.tsx
@@ -1,5 +1,5 @@
 import styles from './Heading.module.css';
 
 export function Heading() {
-    return <h1 className={styles.cyan}>Ola mundo!</h1>
+    return <h1>Ola mundo!</h1>
 }
