dba28a0741f81eca82aa6e868cac312bdebc20fa
Rodrigo Lomardo Xisto <rlomardo@detro.rj.gov.br>
2025-06-11 10:04:51 -0300

    Configuração e desconstrução do elemento Heading

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import {  TimerIcon } from 'lucide-react';
 import { Heading } from './components/Heading';
 import './styles/global.css';
 import './styles/theme.css';
@@ -6,12 +7,18 @@ export function App() {
 
 
     return (
-        <div>
-            <Heading>
+        <>
+            <Heading>Olá Mundo 1
+
+                <button>
+                    <TimerIcon />
+                </button>
+
+
             </Heading>
 
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio voluptatem corrupti quo itaque rem, in quam dignissimos iste? Ullam quae minus non eum. Cupiditate aspernatur nisi vitae dolore laboriosam? Tempora!</p>
-        </div>
+        </>
     )
 
 }
