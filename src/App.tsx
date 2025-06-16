95928c522a2164ed2d869a438026844a2af7fce2
Rodrigo Lomardo Xisto <rlomardo@detro.rj.gov.br>
2025-06-16 10:08:15 -0300

    Criação do componente de containe

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import { Container } from './components/Container';
 import './styles/global.css';
 import './styles/theme.css';
 
@@ -6,34 +7,14 @@ export function App() {
 
     return (
         <>
-            <div className="container">
-                <div className="content"> 
-                    <section>
-                        LOGO
-                    </section>
-                </div>
-            </div>
-            <div className="container">
-                <div className="content"> 
-                    <section>
-                        MENU
-                    </section>
-                </div>
-            </div>
-            <div className="container">
-                <div className="content"> 
-                    <section>
-                        FORM
-                    </section>
-                </div>
-            </div>
-            <div className="container">
-                <div className="content"> 
-                    <section>
-                        FOOTER
-                    </section>
-                </div>
-            </div>
+            <Container>
+                <section>Logo</section>
+            </Container>
+            <Container>
+                <section>MENU</section>
+            </Container>
+           
+           
         </>
     )
 
